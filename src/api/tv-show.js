4925da3f0fa2a3ee2f9a215af517372c29a27594
noumbissi16 <noumbissistael@gmail.com>
2023-08-15 02:25:27 +0100

    Added bearer

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -6,7 +6,8 @@ const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: process.env.REACT_APP_BEARER_KEY,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNWZjN2QxMTY2ZDQ4ZTc1MDYxYjQ5MDA2OTIxNDFiMCIsInN1YiI6IjY0ZDY5MzE4ZDA1MWQ5MDBhZjM0OWRmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.29O6k26mRPj2ek1XCwPRd8VDP34tptRJcxHQUMEjR3U",
   },
 };
 const url = "https://api.themoviedb.org/3/movie/popular?language=fr-FR&page=1";
